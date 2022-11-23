f3c5efc600b18c4f532f1128f24ca94e972fce44
sooni2 <jinong22@gmail.com>
2022-11-23 19:38:51 +0900

    java programmers lv.1 - 하샤드 수

---
+++ src/harshadNumber.java
@@ -0,0 +1,12 @@
+public class harshadNumber {
+    public boolean solution(int x) {
+        boolean answer = true;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        harshadNumber method = new harshadNumber();
+        System.out.println(method.solution(10));
+    }
+}
