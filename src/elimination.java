0df4dbb42fa45f12e26b474056b4fd337eac48ea
sooni2 <jinong22@gmail.com>
2022-11-23 10:02:22 +0900

    java programmers lv.1 - 제일 작은 수 제거하기

---
+++ src/elimination.java
@@ -0,0 +1,11 @@
+public class elimination {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        elimination method = new elimination();
+        System.out.println(method.solution(new int[]{10}));
+    }
+}
