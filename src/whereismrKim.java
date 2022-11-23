fc43940fe03628be2a818d1ce554efece82e69bb
sooni2 <jinong22@gmail.com>
2022-11-23 09:54:32 +0900

    java programmers lv.1 - 서울에서 김서방 찾기

---
+++ src/whereismrKim.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class whereismrKim {
+    public static void main(String[] args) {
+        String[] seoul ={};
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
