d7e11696532af51baa3605b307054e782460d981
sooni2 <jinong22@gmail.com>
2022-11-23 10:01:48 +0900

    java programmers lv.1 - 내적

---
+++ src/naejuk.java
@@ -0,0 +1,12 @@
+public class naejuk {
+    public static void main(String[] args) {
+        int answer = 0;
+        int[] a ={1,2,3,4};
+        int[] b ={-3,-1,0,2};
+        for(int i=0; i<a.length; i++){
+                answer +=(a[i]*b[i]);
+            }
+        System.out.println(answer);
+        }
+
+    }
