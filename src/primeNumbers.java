041a5ff8d6feaa703fe318af24355a007503e981
sooni2 <jinong22@gmail.com>
2022-11-23 09:42:36 +0900

    알고리즘 모의고사 - 소수의 개수와 덧셈

---
+++ src/primeNumbers.java
@@ -0,0 +1,16 @@
+public class primeNumbers {
+
+    public String solution(String s) {
+            String answer = "";
+
+            return answer;
+        }
+
+
+        public static void main(String[] args) {
+            primeNumbers method = new primeNumbers();
+            String s = "97 75 88 99 95 92 73";
+            System.out.println(method.solution(s));
+        }
+}
+
