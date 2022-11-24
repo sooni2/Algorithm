7e1fced418481db5693216cc4b65467fbf37027d
sooni2 <jinong22@gmail.com>
2022-11-24 16:50:45 +0900

    java programmers lv.1 - 하샤드 수 풀이

--- src/harshadNumber.java
+++ src/harshadNumber.java
@@ -2,11 +2,26 @@ public class harshadNumber {
     public boolean solution(int x) {
         boolean answer = true;
 
-        return answer;
+        String num = String.valueOf(x);
+        String[] numbers = num.split("");
+
+        int sum =0;
+
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+
+        if(x%sum==0){
+            return answer;
+        } else{
+            answer=false;
+            return answer;
+        }
+
     }
 
     public static void main(String[] args) {
         harshadNumber method = new harshadNumber();
-        System.out.println(method.solution(10));
+        System.out.println(method.solution(18));
     }
 }
