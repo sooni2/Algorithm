8a648ae248335aee5aed4decfeea946199c558a6
sooni2 <jinong22@gmail.com>
2022-12-08 01:32:19 +0900

    프로그래머스 lv.2 게임 맵 최단거리

---
+++ src/gamemapShortcut.java
@@ -0,0 +1,13 @@
+public class gamemapShortcut {
+
+        public int solution(int[][] maps) {
+            int answer = 0;
+            return answer;
+        }
+
+//    public static void main(String[] args) {
+//        gamemapShortcut method = new gamemapShortcut();
+//        System.out.println(method.solution("[[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]"));
+//    }
+}
+
